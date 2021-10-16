ef94f3ae04eab9cfcdc647f49f253d4c8faa8d4f
unknown <dinkymevawala.dcs22n@vnsgu.ac.in>
2021-10-16 12:36:52 +0530

    Final Submit

---
+++ Questions.js
@@ -0,0 +1,54 @@
+const questions = [
+    {
+      question : "What oil is used for cooking in South India?",
+      answer : "Coconut oil",
+      options: ["Coconut oil","Sunflower Oil","Olive Oil","None of these"]
+    },
+    {
+      question : "Which is the national vegetable of India?",
+      answer : "Meetha Kaddu",
+      options: ["lady’s-finger","Brinjal","Meetha Kaddu","None of these"]
+    },
+    {
+      question : "Which is the national food of India?",
+      answer : "Khichdi",
+      options: ["Khichdi","Roti","Dal","None of these"]
+    },
+    {
+      question : "Which Vitamins are rich in Carrots?",
+      answer : "all of above",
+      options: ["Vitamin A","Vitamin K1","Vitamin B6","all of above"]
+    },
+    {
+      question : "Strawberry is good source of which vitamin?",
+      answer : "Vitamin C",
+      options: ["Vitamin C","Vitamin A","Vitamin K","None of these"]
+    },
+    {
+      question : "Lectin protein is found in ____.",
+      answer : "Wheat",
+      options: ["Wheat","Rice","Milk","None of these"]
+    },
+    {
+      question : "Though ‘Sambar’ is popular across South India, which state is most famous for ‘Sambar’?",
+      answer : "Tamil Nadu",
+      options: ["Tamil Nadu","Kerala","Andhra Pradesh","None of these"]
+    },
+    {
+      question : "Where does the Caesar salad originate from?",
+      answer : "Mexico",
+      options: ["Canada","Italy","Mexico","None of these"]
+    },
+    {
+      question : "Where does coffee originate from?",
+      answer : "Ethiopia",
+      options: ["Canada","Italy","Ethiopia","None of these"]
+    },
+    {
+      question : "Common food sources of Vitamin A are:-",
+      answer : "Milk, eggs, butter, cheese, cream",
+      options: ["papayas, broccoli, tomatoes, kale","Milk, eggs, butter, cheese, cream","Sugar, Sugar cane","None of these"]
+    },
+  ]
+  
+  module.exports = questions;
